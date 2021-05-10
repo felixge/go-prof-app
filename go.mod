9d830ee0295993e533e52f4047aaa3759b5a25df
Felix Geisendörfer <felix@felixge.de>
2021-05-10 16:05:27 +0200

    WIP

--- go.mod
+++ go.mod
@@ -2,4 +2,13 @@ module github.com/felixge/go-prof-app
 
 go 1.16
 
-require github.com/julienschmidt/httprouter v1.3.0
+require (
+	github.com/Microsoft/go-winio v0.5.0 // indirect
+	github.com/google/uuid v1.2.0 // indirect
+	github.com/julienschmidt/httprouter v1.3.0
+	github.com/opentracing/opentracing-go v1.2.0 // indirect
+	github.com/philhofer/fwd v1.1.1 // indirect
+	golang.org/x/time v0.0.0-20210220033141-f8bda1e9f3ba // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	gopkg.in/DataDog/dd-trace-go.v1 v1.30.1-0.20210507131049-ae70d0d7c146
+)
--- main.go
+++ main.go
@@ -4,11 +4,13 @@ import (
 	_ "embed"
 	"flag"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 	"strings"
 
 	"github.com/julienschmidt/httprouter"
+	"gopkg.in/DataDog/dd-trace-go.v1/profiler"
 )
 
 var (
@@ -26,9 +28,37 @@ func main() {
 
 func run() error {
 	var (
-		addrF    = flag.String("addr", "localhost:8080", "Listen addr for http server")
-		versionF = flag.Bool("version", false, "Print version and exit")
+		profiles          = []profiler.ProfileType{profiler.CPUProfile, profiler.HeapProfile}
+		availableProfiles = map[string]profiler.ProfileType{
+			"cpu":       profiler.CPUProfile,
+			"heap":      profiler.HeapProfile,
+			"block":     profiler.BlockProfile,
+			"mutex":     profiler.MutexProfile,
+			"goroutine": profiler.GoroutineProfile,
+		}
+
+		addrF         = flag.String("addr", "localhost:8080", "Listen addr for http server")
+		envF          = flag.String("env", "dev", "Name of the environment the app is running in")
+		ddKey         = flag.String("dd.key", "", "API key for dd-trace-go agentless profile uploading")
+		ddPeriod      = flag.Duration("dd.period", profiler.DefaultPeriod, "Profiling period for dd-trace-go")
+		ddCPUDuration = flag.Duration("dd.cpuDuration", profiler.DefaultDuration, "CPU duration for dd-trace-go")
+		versionF      = flag.Bool("version", false, "Print version and exit")
 	)
+	flag.Func("dd.profiles", `Comma separated list of dd-trace-go profiles to enable (default "cpu,heap")`, func(val string) error {
+		profiles = nil
+		val = strings.TrimSpace(val)
+		if val == "" {
+			return nil
+		}
+		for _, name := range strings.Split(val, ",") {
+			profile, ok := availableProfiles[name]
+			if !ok {
+				return fmt.Errorf("unknown profile type: %q", name)
+			}
+			profiles = append(profiles, profile)
+		}
+		return nil
+	})
 	flag.Parse()
 
 	if *versionF {
@@ -36,7 +66,36 @@ func run() error {
 		return nil
 	}
 
+	log.Println("Starting up")
+	var profilesS []string
+	for _, p := range profiles {
+		profilesS = append(profilesS, p.String())
+	}
+	log.Printf("Enabled profiles: %v", profilesS)
+
+	profilerOptions := []profiler.Option{
+		profiler.WithService("go-prof-app"),
+		profiler.WithEnv(*envF),
+		profiler.WithVersion(version),
+		profiler.WithProfileTypes(profiles...),
+		profiler.CPUDuration(*ddCPUDuration),
+		profiler.WithPeriod(*ddPeriod),
+	}
+	if *ddKey != "" {
+		log.Printf("Using agentless uploading")
+		profilerOptions = append(
+			profilerOptions,
+			profiler.WithAPIKey(*ddKey),
+			profiler.WithAgentlessUpload(),
+		)
+	}
+	err := profiler.Start(profilerOptions...)
+	if err != nil {
+		return err
+	}
+	defer profiler.Stop()
+
 	router := httprouter.New()
-	router.Handler("GET", "/version", VersionHandler{Version: version})
+	router.Handler("GET", "/", VersionHandler{Version: version})
 	return http.ListenAndServe(*addrF, router)
 }
